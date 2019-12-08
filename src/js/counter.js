3d29eac960e993c75b3c922c334aeda804411e7a
mirexdoors <mirex.doors@mail.ru>
2019-12-08 14:28:02 +0700

    add toolbar

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,18 +1,4 @@
 function setCounter() {
-  let counterData = 0;
-  const counterView = document.createElement(`H1`);
-  const counterBtn = document.createElement(`BUTTON`);
-  counterBtn.innerText = `Click Me!!`;
-  document.body.appendChild(counterBtn);
-
-  counterBtn.addEventListener(`click`, () => {
-    counterData += 1;
-    counterView.innerText = counterData;
-  });
-
-
-  counterView.innerText = counterData;
-  document.body.appendChild(counterView);
 }
 
 export default setCounter;
