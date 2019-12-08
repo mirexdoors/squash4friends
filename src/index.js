74bdb0016149685b4d6e1bdd835cb49585ccedc9
mirexdoors <mirex.doors@mail.ru>
2019-12-08 13:56:12 +0700

    add counter in toolbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './css/style.css';
-import setCounter from './js/counter';
 
-setCounter();
-text();
 
 document.addEventListener(`DOMContentLoaded`, () => {
-  const hello = `Hello world!!`;
-  console.log(hello);
+
 });
