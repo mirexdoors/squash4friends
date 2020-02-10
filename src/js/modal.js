8d0665daa31cff1ac01d1fab2f85baeecf04d6c6
mirexdoors <mirex.doors@mail.ru>
2020-02-11 02:25:22 +0700

    forms media styles

--- src/js/modal.js
+++ src/js/modal.js
@@ -19,32 +19,7 @@ $(`.js-formClose`).on(`click`, function () {
     $(`body`).removeClass(`darken--mobile`);
 });
 
-$(`.form`).on(`submit`, function (e) {
-    console.log(this);
-    e.preventDefault();
-    /*валидация*/
-    let isError = false;
-    const requiredFields = $(this).find(`.required`);
-    requiredFields.each((item) => {
-        const input = $(requiredFields[item]);
-        if (input.val().length < 1) {
-            input.parent().addClass(`error`);
-            isError = true;
-        }
-    });
-    if (!isError) {
-        if ($(this).hasClass(`modalForm`)) {
-            $(this).fadeOut();
-        } else {
-            this.reset();
-            $(`.darkenMask`).show();
-            $(`body`).addClass(`darken`);
-            $(`body`).addClass(`darken--mobile`);
-        }
-        $(`#success`).fadeIn();
 
-    }
-});
 
 /*ввод в инпут*/
 $(`input.required`).on(`input`, function () {
