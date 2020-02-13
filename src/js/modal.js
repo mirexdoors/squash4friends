92e127def504f4b396d1fc4a9eabf11c7672af20
mirexdoors <mirex.doors@mail.ru>
2020-02-14 01:22:22 +0700

    add logic inn binded selects

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,38 +1,54 @@
 import $ from "jquery";
 
+function getCookie(name) {
+  const matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function createCookie(name, value, hours) {
+  let expires = ``;
+  if (hours) {
+    const date = new Date();
+    date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+    expires = `; expires=${date.toGMTString()}`;
+  }
+  document.cookie = `${name}=${value + expires}; path=/`;
+}
+
 $(`.js-openModal`).on(`click`, function () {
-    const modalId = $(this).data(`modal`);
-    if (modalId && $(modalId).length > 0) {
-        $(`${modalId}`).fadeIn();
-        $(`.darkenMask`).show();
-        $(`body`).addClass(`darken`);
-        $(`body`).addClass(`darken--mobile`);
-        window.isDarken = true;
-    }
+  const modalId = $(this).data(`modal`);
+  if (modalId && $(modalId).length > 0) {
+    $(`${modalId}`).fadeIn();
+    $(`.darkenMask`).show();
+    $(`body`).addClass(`darken`);
+    $(`body`).addClass(`darken--mobile`);
+    window.isDarken = true;
+  }
 });
 
 $(`.js-formClose`).on(`click`, function () {
-    const form = $(this).parent();
-    form.fadeOut();
-    $(`.darkenMask`).hide();
-    $(`body`).removeClass(`darken`);
-    $(`body`).removeClass(`darken--mobile`);
+  const form = $(this).parent();
+  form.fadeOut();
+  $(`.darkenMask`).hide();
+  $(`body`).removeClass(`darken`);
+  $(`body`).removeClass(`darken--mobile`);
 });
 
 /* ввод в инпут */
 $(`input.required`).on(`input`, function () {
-    if ($(this).parent().hasClass(`error`)) {
-        $(this).parent().removeClass(`error`);
-    }
+  if ($(this).parent().hasClass(`error`)) {
+    $(this).parent().removeClass(`error`);
+  }
 });
 /* проверяем localstorage на показ блока из ИГ */
-if (!localStorage.getItem(`isShowInsta`)) {
-    setTimeout(() => {
-        $(`.darkenMask`).show();
-        $(`body`).addClass(`darken`);
-        window.isDarken = true;
-        $('#insta-modal').fadeIn();
-    }, 5000);
-    //записываем в loalstorage после показа
-  localStorage.setItem('isShowInsta', true);
+if (!getCookie(`isShowInsta`)) {
+  setTimeout(() => {
+    $(`.darkenMask`).show();
+    $(`body`).addClass(`darken`);
+    window.isDarken = true;
+    $(`#insta-modal`).fadeIn();
+  }, 5000);
+  createCookie(`isShowInsta`, true, 3);
 }
--- src/js/select.js
+++ src/js/select.js
@@ -1,5 +1,15 @@
 import $ from 'jquery';
 
+// закрытие селектов по клику вне
+$(document).click((e) => {
+  const selects = $(`.js-pseudo-select`);
+  $.each(selects, function () {
+    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $(this).parent().find(`.selectBox__pseudoList`).hasClass(`isOpen`)) {
+      $(this).parent().find(`.selectBox__pseudoList`).toggleClass(`isOpen`);
+    }
+  });
+});
+
 $(`.js-pseudo-select`).on(`click`, function () {
   $(this).parent().find(`.selectBox__pseudoList`).toggleClass(`isOpen`);
 });
@@ -14,6 +24,45 @@ $(`.js-pseudo-options`).on(`click`, `li`, function () {
   // меняем псевдоселект
   parentBox.find(`.js-pseudo-select`).html(value.parent().html());
   parentBox.find(`.js-pseudo-select`).removeClass(`selectBox__pseudoCurrent--placeholder`);
-  // закрываем окно
+
   $(this).parent().toggleClass(`isOpen`);
+
+  /* смена связанных селектов */
+  const countryId = $(this).data(`id`);
+  if (parentBox.find(`select[name="country"]`).length) {
+    const cityBlockSelect = parentBox.parent().find(`select[name="city"]`);
+    if (cityBlockSelect.length) {
+      /* Показываем нужные элементы */
+      const cityParentBox = cityBlockSelect.parents('.selectBox');
+      const pseudoCities = cityParentBox.find('.js-pseudo-options li');
+      const cityPseudoSelect = cityParentBox.find('.js-pseudo-select');
+      let isFirst = false;
+      let firstCity = ``;
+      $.each(pseudoCities, function () {
+        if ($(this).data(`id`) === countryId) {
+          $(this).show();
+          if (!isFirst) {
+            firstCity = $(this).find(`.selectBox__value`).text();
+            isFirst = true;
+          }
+        } else {
+          $(this).hide();
+        }
+      });
+
+      if (firstCity.length) {
+        /* Показываем в селекте городов первый элемент */
+        if (cityPseudoSelect.find(`.selectBox__value`).length) {
+          cityParentBox.find(`.js-pseudo-select`).find(`.selectBox__value`).text(firstCity);
+        } else {
+          cityPseudoSelect.removeClass(`selectBox__pseudoCurrent--placeholder`);
+          cityPseudoSelect.text(firstCity);
+        }
+
+
+        /* Меняем нативный селект */
+        cityBlockSelect.val(firstCity);
+      }
+    }
+  }
 });
